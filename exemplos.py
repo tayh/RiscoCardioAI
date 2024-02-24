ffc74494fde7a1067e22cfe7fd230461e21c9475
Taynara Carvalho <tayhcarvalho@gmail.com>
2024-02-24 13:12:22 -0300

    refactor: move examples to another file

---
+++ exemplos.py
@@ -0,0 +1,5 @@
+exemplos = {
+    "Exemplo 1": {"name": "João Silva", "idade": "30", "prontuario": "Paciente com histórico de hipertensão."},
+    "Exemplo 2": {"name": "Maria Oliveira", "idade": "45", "prontuario": "Paciente com histórico de diabetes tipo 2."},
+    "Exemplo 3": {"name": "Marcos Silva", "idade": "60", "prontuario": "Paciente com histórico doenças cardíacas."},
+}
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
 import streamlit as st
+from exemplos import exemplos  # Importe o dicionário de exemplos
 
-st.text_input("Nome", key="name")
+# Criação do menu lateral
+#st.sidebar.image("caminho_para_seu_logo.png", caption="RiscoCardioAI")
+exemplo_selecionado = st.sidebar.selectbox("Exemplos", options=list(exemplos.keys()))
 
-idade = st.text_input("Idade", key="idade", max_chars=3)
+# Atualiza os valores dos inputs baseado no exemplo selecionado
+valores_exemplo = exemplos[exemplo_selecionado]
+nome = st.text_input("Nome", key="name", value=valores_exemplo["name"])
+idade = st.text_input("Idade", key="idade", value=valores_exemplo["idade"], max_chars=3)
 
 # Verificar se a idade é um número
 if idade and not idade.isnumeric():
     st.error("Por favor, insira apenas números para a idade.")
 
-st.text_area("Prontuário", key="prontuario", height=300)
+prontuario = st.text_area("Prontuário", key="prontuario", height=300, value=valores_exemplo["prontuario"])
